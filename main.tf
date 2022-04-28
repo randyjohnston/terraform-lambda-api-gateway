f745b0e2e384b0105d1cef7687a8262b4eb4c086
Randy Johnston <randyjohnston@gmail.com>
2022-04-28 12:51:20 -0500

    Adds s3 bucket encryption

--- main.tf
+++ main.tf
@@ -32,3 +32,19 @@ resource "aws_s3_bucket" "lambda_bucket" {
   acl           = "private"
   force_destroy = true
 }
+
+resource "aws_kms_key" "code_key" {
+  description             = "This key is used to encrypt code s3 objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.lambda_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.code_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
